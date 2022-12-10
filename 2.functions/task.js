704763de68b438a484bca0ede63e1c48db473352
Vladimir Kalinin <Vofanchic@mail.ru>
2022-12-10 20:44:52 +0500

    case 1 added

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,16 @@
 function getArrayParams(...arr) {
-
+  let min = Infinity, max = -Infinity, sum = 0;
+  for (let i = 0; i<arr.length; i++){
+    if (arr[i]>max){
+      max = arr[i];
+    }
+    if (arr[i]<min){
+      min = arr[i];
+    }
+    sum+=arr[i]
+  }
+  let avg = sum/arr.length
+  avg = +avg.toFixed(2)
   return { min: min, max: max, avg: avg };
 }
 
