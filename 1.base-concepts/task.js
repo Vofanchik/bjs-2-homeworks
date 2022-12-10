dd1e35e534d98d024182e9abc6970c5e3de64107
Vladimir Kalinin <Vofanchic@mail.ru>
2022-12-10 17:49:53 +0500

    1 case added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2)-4*a*c;
+  switch (true){
+    case (d<0):
+      break;
+    case (d==0):
+      arr.push(-b/(2*a));
+      break;
+    default:
+      arr.push((-b + Math.sqrt(d)) / (2*a));
+      arr.push((-b - Math.sqrt(d)) /(2*a));    
+  }
   return arr;
 }
 
