aaac9a1a6d020f27d38a68dc05b3b60e3575169f
Vladimir Kalinin <Vofanchic@mail.ru>
2022-12-12 16:19:57 +0500

    all done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((a, b) => a === arr2[b])
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  if ((gender == "мужской" || gender == "женский") && users.length != 0) {  
+    filred = users.filter(x => x.gender === gender)
+    return filred.reduce((a, b) => a + Number(b.age), 0) / filred.length }
+  else {return 0}}
