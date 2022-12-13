19638f75b23a4959d31c1466d5bd1fb13bcbe1c1
Vladimir Kalinin <Vofanchic@mail.ru>
2022-12-13 23:39:00 +0500

    4 started

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,13 +1,26 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+let student1 = new Student("Anton", "M", "19");
+let student2 = new Student("Andrey", "M", "20");
+let student3 = new Student("Artem", "M", "21");
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks){
+    for (let mark of marks){
+
+        this.marks.push(mark)
+    }
+  }
+  else {console.log('студент отчислен')}
 }
 
 Student.prototype.getAverage = function () {
